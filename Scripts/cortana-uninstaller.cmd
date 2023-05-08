f34670de9a9dd44ac4c47b5ca356abe6f10ff871
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-05-08 22:19:21 +0300

    Update cortana-uninstaller.cmd

--- Scripts/cortana-uninstaller.cmd
+++ Scripts/cortana-uninstaller.cmd
@@ -7,6 +7,7 @@ Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowC
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "ConnectedSearchUseWeb" /t REG_DWORD /d "0" /f> nul
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowSearchToUseLocation" /t REG_DWORD /d "0" /f> nul
 Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "DisableWebSearch" /t REG_DWORD /d "1" /f> nul
+powershell -command "Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Search -Name "BingSearchEnabled" -Value 1 -Type DWord"
 echo Hiding Cortana From Taskbar
 Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowCortanaButton" /t REG_DWORD /d "0" /f> nul
 echo Uninstalling Cortana
