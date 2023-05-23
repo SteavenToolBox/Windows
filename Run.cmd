4305cf98b9526e827d7028c9f7549fa829307bbc
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-05-23 16:07:25 +0300

    Update Run.cmd

--- Run.cmd
+++ Run.cmd
@@ -1,6 +1,7 @@
 echo off
 cls
 ver | findstr /i "10\.0\.19045\." > nul && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox.ps1 | iex")
+ver | findstr /i "10\.0\.22621\." > nul && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox.ps1 | iex")
 ver | findstr /i "6\.1\." > nul  && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox7.ps1 | iex")
 echo "Could not detect Windows version! exiting..."
 color 4F & pause & exit /B 1
