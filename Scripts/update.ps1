9f1ce898c857a90eefb6468b47dee301ca2fd08b
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-08-09 07:18:43 +0300

    Update update.ps1

--- Scripts/update.ps1
+++ Scripts/update.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 winget upgrade --all
-choco upgrade all
+choco upgrade all -y
 scoop update
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
