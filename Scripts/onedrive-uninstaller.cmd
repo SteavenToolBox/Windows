9182a405ee3199473fa6c1ce2d28d254413793ff
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-08-10 18:59:05 +0300

    typo

--- Scripts/onedrive-uninstaller.cmd
+++ Scripts/onedrive-uninstaller.cmd
@@ -36,7 +36,7 @@ taskkill /f /im OneDrive.exe> nul
    rd "C:\OneDriveTemp" /s /q> nul
    del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" /s /f /q> nul
    echo Removing OneDrive registry keys.  
-   REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f> nul
-   REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f> nul
-   REG ADD "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v System.IsPinnedToNameSpaceTree /d "0" /t REG_DWORD /f> nul
+   reg Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f> nul
+   reg Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f> nul
+   reg ADD "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v System.IsPinnedToNameSpaceTree /d "0" /t REG_DWORD /f> nul
 pause
