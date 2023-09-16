b1fa44f235290e5cefc12fd0e01c6fe6d1e8f6d0
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-09-16 20:12:24 +0300

    updated edge uninstaller + onedrive uninstaller

--- Scripts/onedrive-uninstaller.cmd
+++ Scripts/onedrive-uninstaller.cmd
@@ -26,6 +26,9 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Fold
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{F42EE2D3-909F-4907-8871-4C22FC0BF756}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\Documents" /f> nul
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{0DDD015D-B06C-45D5-8C4C-F59713854639}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\Pictures" /f> nul
 taskkill /f /im OneDrive.exe> nul
+echo Using winget to uninstall OneDrive
+winget uninstall Microsoft.OneDrive
+echo Doing it the old way
 %SystemRoot%\System32\OneDriveSetup.exe /uninstall> nul
 %SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall> nul
    echo Removing remaining OneDrive folders.
