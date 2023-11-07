99ab2225c215dd2bc7a2263b08f42482484ff46c
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-11-08 00:04:28 +0200

    Added Support for 23h2

--- Run.cmd
+++ Run.cmd
@@ -3,4 +3,5 @@ cls
 ver | findstr /i "10\.0\.19044\." > nul && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox.ps1 | iex")
 ver | findstr /i "10\.0\.19045\." > nul && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox.ps1 | iex")
 ver | findstr /i "10\.0\.22621\." > nul && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox.ps1 | iex")
+ver | findstr /i "10\.0\.22631\." > nul && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox.ps1 | iex")
 ver | findstr /i "6\.1\." > nul  && (powershell -command "irm github.com/SteavenToolBox/Windows/raw/main/SteavenToolBox7.ps1 | iex")
