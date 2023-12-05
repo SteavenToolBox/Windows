9f3402c4b14b837506854b1ee264b66d961e882f
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-12-05 23:50:21 +0200

    Update runtime.ps1

--- Scripts/runtime.ps1
+++ Scripts/runtime.ps1
@@ -66,6 +66,12 @@ function Install-Scoop {
 
 
 function Install-WindowsUpdateCli {
+    # Store the current ConfirmPreference
+    $currentConfirmPreference = $ConfirmPreference
+
+    # Set the ConfirmPreference to "Yes" only for this function
+    $ConfirmPreference = 'Yes'
+
     # Check if PSWindowsUpdate module is installed
     if (-not (Get-Module -Name PSWindowsUpdate -ListAvailable)) {
         Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
@@ -76,6 +82,9 @@ function Install-WindowsUpdateCli {
     if (-not ($wuService.MicrosoftUpdate -eq $true)) {
         Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
     }
+
+    # Restore the original ConfirmPreference
+    $ConfirmPreference = $currentConfirmPreference
 }
 
 
